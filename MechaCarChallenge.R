73db070c4f72cec3927cf7f41910860d0e8e6d82
Franklin Tello <franklintello@msn.com>
2023-04-03 18:27:11 -0700

    Fixing errors on final code

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -5,9 +5,9 @@ library(dplyr)
 #Import and read in the MechaCar_mpg.csv
 MechaCar_mpg <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 #create multiple linear model
-lm(vehicle_length ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg,data=MechaCar_mpg)
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_mpg)
 #generate summary stats
-summary(lm(vehicle_length ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg,data=MechaCar_mpg))
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_mpg))
 
 
 
